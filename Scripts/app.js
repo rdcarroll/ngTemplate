c728667be1af82d783b5ff548bd99b7f362d289d
rdcarroll <rdcarroll_utk@yahoo.com>
2015-01-12 14:00:40 -0500

    updates

--- Scripts/app.js
+++ Scripts/app.js
@@ -13,24 +13,23 @@ angular.module('app', ['ui.router', 'app.filters', 'app.services', 'app.directiv
         $stateProvider
             .state('home', {
                 url: '/',
-                templateUrl: '/views/index',
+                templateUrl: '/client/src/app/home/home.tpl.html',
                 controller: 'HomeCtrl'
 
             })
-            .state('about', {
-                url: '/about',
-                templateUrl: '/views/about',
+            .state('contacts', {
+                url: '/contacts',
+                    templateUrl: '/client/src/app/about/about.tpl.html',
                 controller: 'AboutCtrl'
-            })
+        })
             .state('login', {
                 url: '/login',
-                layout: 'basic',
-                templateUrl: '/views/login',
+                    templateUrl: '/client/src/app/home/home.tpl.html',
                 controller: 'LoginCtrl'
             })
             .state('otherwise', {
                 url: '*path',
-                templateUrl: '/views/404',
+                    templateUrl: '/client/src/app/home/home.tpl.html',
                 controller: 'Error404Ctrl'
             });
 
@@ -42,19 +41,4 @@ angular.module('app', ['ui.router', 'app.filters', 'app.services', 'app.directiv
     // can be injected here. This is to prevent further system configuration during application run time.
     .run(['$templateCache', '$rootScope', '$state', '$stateParams', function ($templateCache, $rootScope, $state, $stateParams) {
 
-        // <ui-view> contains a pre-rendered template for the current view
-        // caching it will prevent a round-trip to a server at the first page load
-        var view = angular.element('#ui-view');
-        $templateCache.put(view.data('tmpl-url'), view.html());
-
-        // Allows to retrieve UI Router state information from inside templates
-        $rootScope.$state = $state;
-        $rootScope.$stateParams = $stateParams;
-
-        $rootScope.$on('$stateChangeSuccess', function (event, toState) {
-
-            // Sets the layout name, which can be used to display different layouts (header, footer etc.)
-            // based on which page the user is located
-            $rootScope.layout = toState.layout;
-        });
     }]);
